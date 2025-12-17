1ea4644ebda184b3b71e05c6bce09906e540548a
moczka <steven.guesp@gmail.com>
2025-12-17 18:05:52 -0500

    Add custom colors

--- app/src/main/java/com/example/connectfourways/MainActivity.kt
+++ app/src/main/java/com/example/connectfourways/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Theme_ConnectFourWays)
+        setTheme(R.style.Theme_Classic)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.nav_host_fragment)) { v, insets ->
