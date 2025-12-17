9ca5145f1c619b506cb3fd8634cfce18514af52b
moczka <steven.guesp@gmail.com>
2025-12-17 14:52:42 -0500

    Add navigation graph configurations

---
+++ app/src/main/java/com/example/connectfourways/GamePlayViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.connectfourways
+
+import androidx.lifecycle.ViewModel
+
+class GamePlayViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/example/connectfourways/MainActivity.kt
+++ app/src/main/java/com/example/connectfourways/MainActivity.kt
@@ -11,23 +11,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.connectfourways.ui.theme.ConnectFourWaysTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            ConnectFourWaysTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+//        setContent {
+//            ConnectFourWaysTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//    }
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.nav_host_fragment)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-    }
 }
 
 @Composable
